94c6ef1c0a4edecde8a8f9e3299a412454826cd0
jojahn <32508536+jojahn@users.noreply.github.com>
2021-08-22 21:27:29 +0200

    fixed dev npm script

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
-"use strict";
 var _a;
 (_a = document.getElementById("Title")) === null || _a === void 0 ? void 0 : _a.insertAdjacentHTML("afterbegin", "<span>Hello, World!</span>");
 console.log("Hello, World!");
+export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import { SimpleGreeting } from "./components/simple-greeting";
+
 document.getElementById("Title")?.insertAdjacentHTML("afterbegin", "<span>Hello, World!</span>");
 console.log("Hello, World!");
